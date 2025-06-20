ab0057a7886e83d691cc8f3ea2eb0513097d60a7
Shadab Faisal <shadabfaisal91@gmail.com>
2025-06-21 01:23:25 +0530

    Created a springboot app for Smart Contact Manager 2.0 using spring initializer

---
+++ Backend/smart-contact-manager-2.0/src/test/java/com/springboot/smart_contact_manager_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.smart_contact_manager_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
